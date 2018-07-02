8d83dd00dcdad94ce3a748f717fc198f7a4d57d4
drivenbits <40771195+drivenbits@users.noreply.github.com>
2018-07-02 19:51:19 -0400

    Add files via upload

---
+++ js/_vti_cnf/bootstrap.js
@@ -0,0 +1,10 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|02 Jul 2018 22:43:49 -0000
+vti_extenderversion:SR|6.0.2.5516
+vti_author:SR|vijaysaki-HP\\vijaysaki
+vti_modifiedby:SR|vijaysaki-HP\\vijaysaki
+vti_nexttolasttimemodified:TR|11 Sep 2015 07:49:35 -0000
+vti_timecreated:TR|02 Jul 2018 22:43:49 -0000
+vti_cacheddtm:TX|02 Jul 2018 22:43:49 -0000
+vti_filesize:IR|55258
+vti_backlinkinfo:VX|index.htm
---
+++ js/_vti_cnf/jquery.bootgrid.js
@@ -0,0 +1,10 @@
+vti_encoding:SR|utf8-nl
+vti_author:SR|vijaysaki-HP\\vijaysaki
+vti_modifiedby:SR|vijaysaki-HP\\vijaysaki
+vti_timelastmodified:TR|02 Jul 2018 22:44:41 -0000
+vti_timecreated:TR|02 Jul 2018 22:32:45 -0000
+vti_extenderversion:SR|6.0.2.5516
+vti_nexttolasttimemodified:TR|02 Jul 2018 22:32:45 -0000
+vti_cacheddtm:TX|02 Jul 2018 22:44:41 -0000
+vti_filesize:IR|70365
+vti_backlinkinfo:VX|index.htm
